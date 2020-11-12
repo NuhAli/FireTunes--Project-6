f311e8f988bb5645aa6b3b4183732863fb9ba279
Nuh Ali <nuh_mali@hotmail.co.uk>
2020-11-12 10:35:25 +0000

    Updates

---
+++ precache-manifest.a689099e382c588237bede01bff33380.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c67a9223ac00002a7daaa0379764a30",
+    "url": "/FireTunes--Project-6/index.html"
+  },
+  {
+    "revision": "b01e4089d33070ea3eb7",
+    "url": "/FireTunes--Project-6/static/css/main.dfae0cd1.chunk.css"
+  },
+  {
+    "revision": "940cc5e3b6ac49f39ccf",
+    "url": "/FireTunes--Project-6/static/js/2.663fcfcc.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/FireTunes--Project-6/static/js/2.663fcfcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b01e4089d33070ea3eb7",
+    "url": "/FireTunes--Project-6/static/js/main.fbaf174c.chunk.js"
+  },
+  {
+    "revision": "fa2ebe2b6e03560309ca",
+    "url": "/FireTunes--Project-6/static/js/runtime-main.75582d76.js"
+  }
+]);
